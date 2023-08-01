a1e2553fb2fccdf47ee65d3eadd68849e3e2facc
Viniciusmg007 <141034413+Viniciusmg007@users.noreply.github.com>
2023-08-01 10:58:48 -0300

    adiciona persistencia em arquivo

--- pokedex.py
+++ pokedex.py
@@ -1,4 +1,18 @@
 pokedex = []
+def listar_arquivo():
+    with open("pokedex.txt", "r") as arquivo:
+        linhas = arquivo.readlines()
+    print("Nome: \t Tipo:")    
+    for linha in linhas:
+        nome, tipo = linha.strip().split('/')
+        print(f"{nome}\t{tipo}")
+
+def cadastrar_arquivo():
+    nome = input("Digite o nome: ")
+    tipo = input("Digite o tipo: ")
+    with open("pokedex.txt", "a") as arquivo:
+	    arquivo.write(f"{nome}/{tipo}\n")
+    
 def cadastrar():
     nome = input("Digite o nome: ")
     tipo = input("Digite o tipo: ")
@@ -16,9 +30,9 @@ while True :
     print(" 1:cadastrar \n 2:listar \n 3:sair")
     opcao = int(input("Escolha uma opção: "))
     if opcao == 1 :
-        cadastrar()
+        cadastrar_arquivo()
     elif opcao == 2 :
-        listar()
+        listar_arquivo()
     else:
         print("Até a proxima.")
         break
